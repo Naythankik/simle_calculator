5aa22438c722f9b78560fd0c906d02d5a1654a23
olawale1678 <imobalaze@gmail.com>
2022-08-26 21:57:10 +0100

    fixed delete button

--- index.js
+++ index.js
@@ -1,79 +1,174 @@
-let buttons = document.getElementsByClassName("button");
-let arr = [...buttons];
+const calc = document.querySelector('.calculator')
+const one = document.getElementById('first')
+const first = document.querySelector('.keyValue')
+const display = document.getElementById('display-context')
+let x;
 
-let themes = ["default-theme", "theme-light", "theme-dark"];
-let bodyContainer = document.querySelector(".body");
+window.onload = () => {
+  display.textContent = 0;
+}
 
-arr.forEach((element, index) => {
-  element.addEventListener("click", () => {
-    bodyContainer.className = themes[index];
-    element.style.opacity = "1";
+calc.addEventListener('click', (e) => {
 
-    arr
-      .filter(function (item) {
-        return item != element;
-      })
-      .forEach((item) => {
-        item.style.opacity = "0";
-      });
-  });
-});
+  const filler = () => {
+    display.textContent += e.target.value
+  }
 
-// The algorithm for calculating the keys pressed
-let displayTab = document.querySelector("#display-context");
-let deleteButton = document.querySelector(".delete");
-let resetButton = document.querySelector(".reset");
-let equalButton = document.querySelector(".equal");
-let decimalButton = document.querySelector(".decimal");
-let keyValues = document.querySelectorAll(".keyValue");
-let operators = document.querySelectorAll(".operator");
+  filler()
 
-// when the reset button is pressed, set the display content to 0
-resetButton.addEventListener("click", function () {
-  displayTab.textContent = "0";
-});
-let valueDisplay = "";
-// for (let i = 0; i < keyValues.length; i++) {
-//   keyValues[i].addEventListener("click", function () {
-//     valueDisplay += keyValues[i].innerHTML;
-//     displayTab.textContent = valueDisplay;
+  if (e.target.textContent === '=') {
+
+    let x = display.textContent;
+    let y = eval(x)
+    display.textContent = y;
+
+  } else if (e.target.textContent === 'RESET') {
+
+    display.textContent = 0
+
+  } else if (e.target.textContent === 'DEL') {
+    display.textContent = display.textContent.substring(0, display.textContent.length - 1);
+  }
+
+  // if (e.target.classList !== 'keyValue' && e.target.textContent === '=') {
+
+  //   let x = display.textContent;
+  //   let y = eval(x)
+  //   display.textContent = y;
+
+  // } else if (e.target.classList === 'reset' && e.target.textContent === 'reset') {
+
+  //   display.textContent = 0
+
+  // } else if (e.target.classList !== 'reset' || e.target.classList !== 'equal' || e.target.classList !== 'delete') {
+  //   function filler() {
+  //     display.textContent += e.target.textContent
+  //   }
+  //   filler()
+  // }
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let buttons = document.getElementsByClassName("button");
+// let arr = [...buttons];
+
+// let themes = ["default-theme", "theme-light", "theme-dark"];
+// let bodyContainer = document.querySelector(".body");
+
+// arr.forEach((element, index) => {
+//   element.addEventListener("click", () => {
+//     bodyContainer.className = themes[index];
+//     element.style.opacity = "1";
+
+//     arr
+//       .filter(function (item) {
+//         return item != element;
+//       })
+//       .forEach((item) => {
+//         item.style.opacity = "0";
+//       });
 //   });
-// }
-
-// equalButton.addEventListener("click", function () {
-//   let valueEntered = displayTab.textContent.split(/[+,-,x,/]/);
-//   console.log(valueEntered);
 // });
 
-keys = document.querySelector(".calculator");
-keys.addEventListener("click", (e) => {
-  if (e.target.matches("button")) {
-    let button = e.target.innerHTML;
-    valueDisplay += button;
-    displayTab.textContent = valueDisplay;
-    switch (button) {
-      case "DEL":
-        console.log("delete");
-        console.log(displayTab.textContent);
-        // displayTab.textContent -= valueDisplay;
-        break;
-      case "x":
-        console.log("Multiply");
-        break;
-      case "+":
-        console.log("add");
-        break;
-      case "-":
-        console.log("Subtract");
-        break;
-      case "/":
-        console.log("Divide");
-        break;
-      case "RESET":
-        console.log("Reset");
-        break;
-      default:
-        "";
-    }
-  }
-});
+// // The algorithm for calculating the keys pressed
+// let displayTab = document.querySelector("#display-context");
+// let deleteButton = document.querySelector(".delete");
+// let resetButton = document.querySelector(".reset");
+// let equalButton = document.querySelector(".equal");
+// let decimalButton = document.querySelector(".decimal");
+// let keyValues = document.querySelectorAll(".keyValue");
+// let operators = document.querySelectorAll(".operator");
+
+// // when the reset button is pressed, set the display content to 0
+// resetButton.addEventListener("click", function () {
+//   displayTab.textContent = "0";
+// });
+// let valueDisplay = "";
+// // for (let i = 0; i < keyValues.length; i++) {
+// //   keyValues[i].addEventListener("click", function () {
+// //     valueDisplay += keyValues[i].innerHTML;
+// //     displayTab.textContent = valueDisplay;
+// //   });
+// // }
+
+// // equalButton.addEventListener("click", function () {
+// //   let valueEntered = displayTab.textContent.split(/[+,-,x,/]/);
+// //   console.log(valueEntered);
+// // });
+
+// keys = document.querySelector(".calculator");
+// keys.addEventListener("click", (e) => {
+//   if (e.target.matches("button")) {
+//     let button = e.target.innerHTML;
+//     valueDisplay += button;
+//     displayTab.textContent = valueDisplay;
+//     switch (button) {
+//       case "DEL":
+//         console.log("delete");
+//         console.log(displayTab.textContent);
+//         // displayTab.textContent -= valueDisplay;
+//         break;
+//       case "x":
+//         console.log("Multiply");
+//         break;
+//       case "+":
+//         console.log("add");
+//         break;
+//       case "-":
+//         console.log("Subtract");
+//         break;
+//       case "/":
+//         console.log("Divide");
+//         break;
+//       case "RESET":
+//         console.log("Reset");
+//         break;
+//       default:
+//         "";
+//     }
+//   }
+// });
